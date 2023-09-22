83b44edb965ba21eff1a8944ad25615ce975495d
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-09-22 11:20:56 +0330

    lint: format all

--- src/api/reading-list/controllers/reading-list.ts
+++ src/api/reading-list/controllers/reading-list.ts
@@ -2,6 +2,6 @@
  * reading-list controller
  */
 
-import { factories } from '@strapi/strapi'
+import {factories} from '@strapi/strapi';
 
 export default factories.createCoreController('api::reading-list.reading-list');
